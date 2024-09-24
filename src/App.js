f0886b25597af70eecbb38c48dab60b25d3adbc6
romualdo.andre <romualdo.andre@sec.ba.gov.br>
2024-09-24 11:34:51 -0300

    formulário de login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootswatch/dist/flatly/bootstrap.css'
+import Login from './views/login'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    numero1: null,
+    numero2: null,
+    resultado: null
+  }
+
+  somar = () => {
+    this.setState({resultado: parseInt(this.state.numero1)+parseInt(this.state.numero2)})
+  }
+
+  render() {
+    return (
+      <div>
+        <Login/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/views/login.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import Card from '../components/card'
+import FormGroup from '../components/form-group';
+
+class Login extends React.Component {
+
+    state = {
+        email: '',
+        senha: ''
+    }
+
+    entrar = () => {
+        console.log('Email', this.state.email)
+        console.log('Senha', this.state.senha)
+    }
+
+    render() {
+        return (<div className='container'>
+            <div className='row'>
+                <div className='col-md-6' style={{ position: 'relative', left: '300px' }}>
+                    <div className="bs-docs-section">
+                        <Card title='Login'>
+                            <div className='row'>
+                                <div className='col-lg-12'>
+                                    <div className='bs-component'>
+                                        <fieldset>
+                                            <FormGroup htmlFor='exampleInputEmail1' label='Email: *'>
+                                                <input type='email' className='form-control' id='exampleInputEmail1'
+                                                    value={this.state.email}
+                                                    onChange={e => this.setState({email: e.target.value})}
+                                                    aria-describedby="emailHelp"
+                                                    placeholder="Digite o Email" />
+                                            </FormGroup>
+                                            <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
+                                                <input type="password"
+                                                    className="form-control"
+                                                    id="exampleInputPassword1"
+                                                    value={this.state.senha}
+                                                    onChange={e => this.setState({senha: e.target.value})}
+                                                    placeholder="Password" />
+                                            </FormGroup>
+                                            <button className="btn btn-success" onClick={this.entrar}>
+                                                <i className="pi pi-sign-in"></i>Entrar</button>
+                                            <button
+                                                className="btn btn-danger">
+                                                <i className="pi pi-plus"></i>  Cadastrar
+                                            </button>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                            </div>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+        </div>)
+    }
+}
+
+export default Login;
